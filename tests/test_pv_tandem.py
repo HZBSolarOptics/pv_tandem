ce65909366d1d2df0847ea3eeefb1683473e2025
P-Tillmann <Peter.tillmann@fu-berlin.de>
2023-05-06 18:20:40 +0200

    added first real unittests for clac_current function

--- tests/test_pv_tandem.py
+++ tests/test_pv_tandem.py
@@ -3,9 +3,10 @@
 """Tests for `pv_tandem` package."""
 
 import pytest
+import pandas as pd
+import numpy as np
 
-
-from pv_tandem import pv_tandem
+from pv_tandem.utils import calc_current
 
 
 @pytest.fixture
@@ -22,3 +23,15 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    
+def test_calc_current_single_spec():
+    spec = pd.Series(np.ones(181), index = np.arange(300, 1205, 5))
+    eqe = pd.Series(np.ones(181)*0.8, index = np.arange(300, 1205, 5))
+    
+    assert(round(calc_current(spec, eqe)) == 436)
+
+def test_calc_current_ts_spec():
+    spec = pd.DataFrame(np.ones(shape=(50,181)), columns = np.arange(300, 1205, 5))
+    eqe = pd.Series(np.ones(181)*0.8, index = np.arange(300, 1205, 5))
+    
+    assert(all(calc_current(spec, eqe).round() == (np.ones(50)*436)))
