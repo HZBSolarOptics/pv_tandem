4db7fac8c6930d50c29177228d6edfa2429053c7
Peter Tillmann <Peter.tillmann@mail.de>
2023-05-11 18:06:12 +0200

    Added modules to __init__.py

--- pv_tandem/__init__.py
+++ pv_tandem/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for pv_tandem."""
 
+from pv_tandem import utils
+from pv_tandem import electrical_models
+from pv_tandem import solarcell_models
+
 __author__ = """Peter Tillmann"""
 __email__ = 'Peter.tillmann@helmholtz-berlin.de'
 __version__ = '0.1.0'
+
