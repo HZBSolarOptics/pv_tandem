979d80ceb07d8c7f63015a66f3408367760644b3
P-Tillmann <Peter.tillmann@fu-berlin.de>
2023-05-24 18:42:01 +0200

    WIP: new example for temperature effect in solar cells

--- pv_tandem/electrical_models.py
+++ pv_tandem/electrical_models.py
@@ -99,3 +99,67 @@ class OneDiodeModel:
         )
 
         return res
+
+if __name__ == "__main__":
+    
+    import matplotlib.pyplot as plt
+    
+    pero_iv = pd.read_csv('../examples/data/pero_iv.csv')
+    
+    pero_cell = OneDiodeModel(tcJsc=-0.001, tcVoc=-0.0013, R_shunt = 1000, R_series=4,
+                             n=1.1, j0=2e-16)
+    
+    res = pero_cell.calc_iv_params(np.array([22]), np.array([25]))
+    
+    iv = pero_cell.calc_iv(np.array([22]), np.array([25]), j_arr=np.linspace(0, 45, 451))
+    
+    df_iv = pd.DataFrame({'voltage':iv[0], 'current':np.linspace(0, 45, 451)})
+    
+    df_iv = df_iv.loc[df_iv['voltage'] > 0]
+    
+    fig, ax = plt.subplots(dpi=150)
+    (pero_iv.set_index('voltage')['current']*-1).plot(ax=ax)
+    df_iv.set_index('voltage')['current'].plot(ax=ax)
+    
+    
+    exp_data = pd.read_csv('../examples/data/pero_voc.csv')
+    exp_data['Intensity'] = (exp_data['Intensity'].astype(float)/10).round()*10
+    exp_data = exp_data.set_index('Intensity').iloc[:,::-1]
+    
+    int_factor = np.repeat([0.1, 0.3, 0.6, 1, 1.2], 3)
+    int_factors = np.array([0.1, 0.3, 0.6, 1, 1.2])
+    
+    
+    int_factors_repeated = np.repeat(int_factors, 3)
+    
+    
+    T_arr = np.array([25, 55, 85])#+273#np.arange(280, 340, 10)
+    T_arr = np.tile(T_arr, 5)
+    
+    j_ph = 22.4 * int_factor
+    
+    V = pero_cell.calc_iv(Jsc=j_ph, cell_temp=T_arr, j_arr=np.linspace(0, 45, 451))
+    
+    V = V.reshape(5,3,-1)
+    
+    fig, ax = plt.subplots(dpi=200)
+    
+    labels = []
+    cycle = ['C0', 'C1', 'C3']
+    for i in range(3):
+        ax.scatter(int_factors*100, V[:,i,0], marker='x', s=80, color=cycle[i])
+
+    exp_data.plot(style='+', ax=ax, markersize=10, color=cycle)
+    
+    ax.legend()
+    
+    ax.set_ylabel('Open curcuit voltage (V)')
+    ax.set_xlabel('Light intensity (% of AM1.5g)')
+    
+    plt.show()
+    
+    pass
+
+    
+
+
