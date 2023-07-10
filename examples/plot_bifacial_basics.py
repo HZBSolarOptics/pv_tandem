dc47ead9fc707648f791dfeace617ab13585b459
Peter Tillmann <Peter.tillmann@mail.de>
2023-07-10 09:43:55 +0200

    Renamed example

--- examples/plot_bifacial_basics.py
+++ examples/plot_bifacial_basics.py
@@ -3,8 +3,21 @@
 """
 Basic examples of bifacial modeling
 ===================================
-Showcasing basic examples for 
+Showcasing basic examples for modeling of bifacial irradiance
 """
+
+# %%
+# Introduction
+# ------------
+# This example shows how to model the irradiance on a bifacial solar cell module,
+# for some examples in Berlin, Germany.
+# The examples are similiar to some of the main figures of the following publication
+#
+# .. [1] P. Tillmann, K. Jäger, A. Karsenti, L. Kreinin, C. Becker (2022)
+#    “Model-Chain Validation for Estimating the Energy Yield of Bifacial 
+#    Perovskite/Silicon Tandem Solar Cells,” Solar RRL 2200079, 
+#    :doi:`10.1002/solr.202200079`
+
 
 from pv_tandem import geo
 import matplotlib.pyplot as plt
@@ -15,22 +28,40 @@ import seaborn as sns
 
 coord_berlin = dict(latitude =52.5, longitude =13.4)
 
+# %%
+# The geometry of the solar cell array and the sun position is defined.
+# The ground between two rows is discretized into n elements, controlled by the
+# parameter ground_steps and defaults to 101. The first example shows the
+# distibution of irradiance of the direct and diffuse components on these 101
+# ground elements for a zenith angle of 31.0 deg and azimuth of 144.1 deg
+
 vf = geo.ModuleIllumination(module_length=1.92,
     module_tilt=52,
     mount_height=0.5,
     module_spacing=7.3,
     zenith_sun=31.9,
-    azimuth_sun=144.1)
+    azimuth_sun=144.1,
+    ground_steps=101)
 
 fig, ax = plt.subplots(dpi=150)
 
 ax.plot(vf.results['radiance_ground_diffuse_emitted']*np.pi*100)
 ax.plot(vf.results['radiance_ground_direct_emitted']*np.pi*100)
 
+ax.legend(['fraction of DHI', 'fraction of DNI'])
+
 ax.set_ylabel('Ground irradiance fraction (%)')
 ax.set_xlabel('Ground array position (-)')
 
 plt.show()
+
+# %%
+# Next we look at how the evolution of the illumination develops during sommer
+# solstice, equinox and winter solstice in Berlin.
+# First the date ranges are defined and the python library pvlib is used to
+# calculate the solar position (zenith and azimuth angle).
+# For each of these days the ground illumination is calucated the the radiance 
+# is converted to irradiance by multipling with pi.
 
 dt_list = [
     pd.date_range("20190620 5:00","20190620 21:00", freq='10min', tz='Europe/Berlin'),
@@ -76,6 +107,13 @@ for i, dt in enumerate(dt_list):
 
 
 fig.tight_layout()
+plt.show()
+
+# %%
+# The last example demonstrates the inhomogenity of the irradiance on the front
+# and backside along the length of the PV module. The number of points for which
+# the irradiance is evaluated along the module is ocntrolled by the parameter
+# module_steps and defaults to 12.
 
 vf = geo.ModuleIllumination(module_length=1.92,
     module_tilt=52,
